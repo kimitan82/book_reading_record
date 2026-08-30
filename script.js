7e2e0f03cee1cc956730b85a96362be4255def66
kimitan82 <kimitan82@gmail.com>
2026-08-30 12:44:36 +0000

    firebaseconfigを記入

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCQCfaJLVW9zwQ0NuSROI_vBgNE4ySmCjA",
+  authDomain: "record-reading-books.firebaseapp.com",
+  projectId: "record-reading-books",
+  storageBucket: "record-reading-books.firebasestorage.app",
+  messagingSenderId: "557327399113",
+  appId: "1:557327399113:web:81b1c213de5281a60f7518",
+  measurementId: "G-DTJDR0P6P2"
 };
 
 const form = document.getElementById("book-form");
